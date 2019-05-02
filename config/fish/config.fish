af5631b1094b128f73eb9a4b21e6eaa56c668e74
Kantouzin <kantouzin0113@gmail.com>
2019-05-02 21:54:11 +0900

    Update settings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,5 +6,9 @@ set -x LC_CTYPE ja_JP.UTF-8
 alias vi="/usr/bin/vim"
 alias ls="ls --all --color --classify"
 if test /usr/bin/trash-put
-alias rm="trash-put"
+    alias rm="trash-put"
 end
+
+# powerline
+set fish_function_path $fish_function_path "/usr/local/lib/python3.6/dist-packages/powerline/bindings/fish/"
+powerline-setup
