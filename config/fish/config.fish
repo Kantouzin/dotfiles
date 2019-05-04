195b991c4b25acc5de4a6c2bb462972773210059
Kantouzin <kantouzin0113@gmail.com>
2019-05-04 23:51:12 +0900

    Update settings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,10 +5,14 @@ set -x LC_CTYPE ja_JP.UTF-8
 # alias
 alias vi="/usr/bin/vim"
 alias ls="ls --all --color --classify"
-alias glgg="git log --oneline --graph --decorate"
 if test /usr/bin/trash-put
     alias rm="trash-put"
 end
+alias glgg="git log --oneline --graph --decorate"
+alias ..2="cd ../.."
+alias ..3="cd ../../.."
+alias cp="cp -i"
+alias mv="mv -i"
 
 # powerline
 set fish_function_path $fish_function_path "/usr/local/lib/python3.6/dist-packages/powerline/bindings/fish/"
